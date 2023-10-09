2147c69a24d00857c87f03e1f2989c1a60e46401
ramalt <ramazanalltuntepe@gmail.com>
2023-10-10 01:17:18 +0300

    feature: OrderDbContext created.
    - Persistence Ignorance applied to order aggregates.

---
+++ Services/OrderService/CourseApp.Order.Infrastructure/OrderDbContext.cs
@@ -0,0 +1,28 @@
+using OrderAggregate = CourseApp.Order.Domain.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseApp.Order.Infrastructure;
+
+public class OrderDbContext : DbContext
+{
+    public const string DEFAULT_SCHEMA = "ordering";
+    public DbSet<OrderAggregate.Order> Orders { get; set; }
+    public DbSet<OrderAggregate.OrderItem> OrderItems { get; set; }
+
+    public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+    {
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.Entity<OrderAggregate.Order>().ToTable("Orders", DEFAULT_SCHEMA);
+        modelBuilder.Entity<OrderAggregate.Order>().OwnsOne(order => order.Address).WithOwner();
+        
+        modelBuilder.Entity<OrderAggregate.OrderItem>().ToTable("Orders", DEFAULT_SCHEMA);
+        modelBuilder.Entity<OrderAggregate.OrderItem>().Property(order => order.Price).HasColumnType("decimal(18,2)");
+
+    }
+
+}
