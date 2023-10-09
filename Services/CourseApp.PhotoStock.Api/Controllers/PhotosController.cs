fcb6188b4b96d4263ce4517711590e4fa25415c2
ramalt <ramazanalltuntepe@gmail.com>
2023-10-09 16:54:22 +0300

    photo endpoints

---
+++ Services/CourseApp.PhotoStock.Api/Controllers/PhotosController.cs
@@ -0,0 +1,43 @@
+using CourseApp.PhotoStock.Api.Dtos;
+using CourseApp.Shared;
+using CourseApp.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using SystemFile = System.IO.File;
+
+namespace CourseApp.PhotoStock.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PhotosController : CustomBaseController
+{
+    [HttpPost]
+    public async Task<IActionResult> SavePhoto(IFormFile file, CancellationToken cancellationToken)
+    {
+        if (file is not null && file.Length > 0)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", file.FileName); // ~/wwwroot/photos/example.jpg
+
+            using Stream stream = new FileStream(path, FileMode.Create);
+
+            await file.CopyToAsync(stream, cancellationToken);
+
+            PhotoDto photoDto = new() { Url = file.FileName };
+            return CreateActionResult(Response<PhotoDto>.Success(photoDto, 200));
+        }
+        return CreateActionResult(Response<PhotoDto>.Fail(200, "photo is empty"));
+    }
+
+    [HttpDelete]
+    public IActionResult DeletePhoto(string url)
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", url); // ~/wwwroot/photos/example.jpg
+
+        if (!SystemFile.Exists(path))
+        {
+            return CreateActionResult(Response<PhotoDto>.Fail(200, "photo not found"));
+        }
+        SystemFile.Delete(path);
+
+        return CreateActionResult(Response<PhotoDto>.Success(200));
+    }
+}
---
+++ Services/CourseApp.PhotoStock.Api/Dtos/PhotoDto.cs
@@ -0,0 +1,6 @@
+namespace CourseApp.PhotoStock.Api.Dtos;
+
+public class PhotoDto
+{
+    public string? Url { get; set; }
+}
