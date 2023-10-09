8d57d9a48bdd8b81ee0e033ef1d61146f6127540
ramalt <ramazanalltuntepe@gmail.com>
2023-10-09 17:30:36 +0300

    payment reveive endpoint

---
+++ Services/CourseApp.Payment.Api/Controllers/FakePaymentController.cs
@@ -0,0 +1,16 @@
+using CourseApp.Shared;
+using CourseApp.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseApp.Payment.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class FakePaymentController : CustomBaseController
+{
+    [HttpPost]
+    public IActionResult ReceivePayment()
+    {
+        return CreateActionResult(Response<NoContent>.Success(200));
+    }
+}
