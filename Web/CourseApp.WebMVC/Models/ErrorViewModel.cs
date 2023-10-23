377404a3f22f3e7bfe9a42d6ec538a8275f3cd8d
ramalt <ramazanalltuntepe@gmail.com>
2023-10-23 22:53:37 +0300

    - Web App Porject create

---
+++ Web/CourseApp.WebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CourseApp.WebMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
